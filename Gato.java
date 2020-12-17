aaafba83a1557aa86bafa60ee903bd82c37cb5d0
Daniel Gutierrez Calzada <danigutcal02@gmail.com>
2020-12-17 12:07:55 +0100

    Commit inicial 2

--- Gato.java
+++ Gato.java
@@ -1,27 +1,27 @@
 public class Gato {
     
-  // El nombre completo del dueño.
-  private String nombreCompletoDueño;
+  // El nombre completo del dueno.
+  private String nombreCompletoDueno;
   // Cantidad (en kiligramos) de pienso que consume el gato a la semana.
   private int cantidadKilogramosPiensoSemana;
   // Vacunación del gato.
   private boolean gatoVacunado;
   /**
-   * Crea una clase en la que se muestra el dueño de un gato, 
+   * Crea una clase en la que se muestra el dueno de un gato, 
    * los kilogramos de pienso que consume a la semana 
-   * y si está vacunado o no.
+   * y si esta vacunado o no.
    */
-  public Gato(String nombreCompletoDelDueño, int cantidadKilogramosDePiensoSemana) {
-      nombreCompletoDueño = nombreCompletoDelDueño;
+  public Gato(String nombreCompletoDelDueno, int cantidadKilogramosDePiensoSemana) {
+      nombreCompletoDueno = nombreCompletoDelDueno;
       cantidadKilogramosPiensoSemana = cantidadKilogramosDePiensoSemana;
       gatoVacunado = true;
   }
   
   /**
-   * Devuelve el nombre completo del dueño.
+   * Devuelve el nombre completo del dueno.
    */
-  public String getNombreCompletoDueño() {
-      return nombreCompletoDueño;
+  public String getNombreCompletoDueno() {
+      return nombreCompletoDueno;
   }
   
   /**
@@ -33,34 +33,29 @@ public class Gato {
   }
   
   /**
-   * Nos da la información nesesaria para saber si el gato está 
+   * Nos da la informacion nesesaria para saber si el gato esta
    * vacunado o no.
    */
   public boolean getGatoVacunado() {
-      if (gatoVacunado == true) {
-          return true;
-      }
-      else {
-          return false;
-      } 
+      return gatoVacunado;
   }    
   
   /**
-   * Fija el valor del atributo nombreCompletoDueño mediante un parámetro.
+   * Fija el valor del atributo nombreCompletoDueño mediante un parametro.
    */
-  public void setNombreCompletoDueño(String nombreCompletoDelDueño) {
-      nombreCompletoDueño = nombreCompletoDelDueño;
+  public void setNombreCompletoDueño(String nombreCompletoDelDueno) {
+      nombreCompletoDueno = nombreCompletoDelDueno;
   }
   
   /**
-   * Modifica mediante un parámetro el valor de cantidadKilogramosPiensoSemana.
+   * Modifica mediante un parametro el valor de cantidadKilogramosPiensoSemana.
    */
   public void comprarKilogramosPienso(int cantidadIntroducida) {
      cantidadKilogramosPiensoSemana = cantidadKilogramosPiensoSemana + cantidadIntroducida;
   } 
   
   /**
-   * Modifica el valor de gatoVacunado cambiándolo alternativamente entre true y false.
+   * Modifica el valor de gatoVacunado cambiandolo alternativamente entre true y false.
    */
   public void vacunarGato() {
       if (gatoVacunado == true) {
@@ -74,17 +69,15 @@ public class Gato {
   /**
    * Imprime por pantalla el estado del objeto.
    */
-   public void imprimeDetalles() {
-      System.out.println("Dueño:" + nombreCompletoDueño);
-      System.out.println("Kilogramos de pienso a la semana:" + cantidadKilogramosPiensoSemana);
-      System.out.println("Vacunado:" + gatoVacunado);
+  public void imprimeDetalles() {
+      System.out.println("Dueno:" + nombreCompletoDueno + "| Kilogramos de pienso a la semana:" + cantidadKilogramosPiensoSemana + "|" + "Vacunado:" + gatoVacunado);
   }
   
   /**
    * Devuelve el contenido del estado del objeto.
    */
   public String devuelveDetalles() {
-        String devuelveDetalles = nombreCompletoDueño  +  cantidadKilogramosPiensoSemana  + gatoVacunado;
+        String devuelveDetalles = ("Dueno:" + nombreCompletoDueno  + "| Kilogramos de pienso a la semana:" +  cantidadKilogramosPiensoSemana + "|" + "Vacunado:" + gatoVacunado);
         return devuelveDetalles;
   }
 }          
